30b6cb746543bc790fd6b8dcd921788365199f67
Zack Levine <znlevine@calpoly.edu>
2014-04-03 12:54:58 -0700

    Adding config and pledge to the internets

---
+++ configureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Zack Levine"
+git config --global user.email "znlevine@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.ditor "nano"
