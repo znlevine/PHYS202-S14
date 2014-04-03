a1c92353bd1f32e4349600f98815c8cd30b15405
Zack Levine <znlevine@calpoly.edu>
2014-04-03 13:05:01 -0700

    Added documentation

--- configureGit.sh
+++ configureGit.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+#NoComment This script configures git whenever you start working
+#NoComment On multiple machines. Run it whenever you want to work
 git config --global user.name "Zack Levine"
 git config --global user.email "znlevine@calpoly.edu"
 git config --global color.ui "auto"
 git config --global core.ditor "nano"
+
